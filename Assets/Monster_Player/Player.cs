2ea71954103c108993438ade94d772f2afb41458
DESKTOP-FU7FP8I\user <a01085357914@gmail.com>
2023-12-22 08:58:44 +0900

    game clear scene

--- Assets/Monster_Player/Player.cs
+++ Assets/Monster_Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -20,6 +21,11 @@ public class Player : MonoBehaviour
             PlayerHP -= 10;
             StartCoroutine(PlayerDamaged());
             Debug.LogWarning(PlayerHP);
+
+            if(PlayerHP <= 0)
+            {
+                SceneManager.LoadScene("GameClearScene");
+            }
             //PlayerDamaged(10);
         }
     }
