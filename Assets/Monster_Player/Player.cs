90f836c691aa3dc28016a74d7e6c8093660b3dda
DESKTOP-FU7FP8I\user <a01085357914@gmail.com>
2023-12-22 00:53:39 +0900

    [ADD] monster asset and animation asset monster attack with animation

--- Assets/Monster_Player/Player.cs
+++ Assets/Monster_Player/Player.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject Earth1;
+    public float PlayerHP = 100.0f;
 
     void Start()
     {
         
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Debug.Log(other.name);
+         
+            PlayerHP -= 10;
+            StartCoroutine(PlayerDamaged());
+            Debug.Log(PlayerHP);
+            //PlayerDamaged(10);
+        }
+    }
 
-    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Instantiate(Earth1, transform.position + transform.forward, transform.rotation);
         }
+    }
+
+    IEnumerator PlayerDamaged()
+    {
+        //yield return new WaitForSeconds(0.2f);
+        //anim.SetBool("Damaged", true);     
+
+        yield return new WaitForSeconds(0.2f);
     }
 }
